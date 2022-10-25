034c9c1216c281b44c9c24ba720bd61dbf4b726f
Masum Billah <masumbillah6584@gmail.com>
2022-10-25 21:23:31 +0600

    update register and login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,55 +1,63 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-      <div>
-        <div className="hero min-h-screen bg-base-100">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <div className="text-center lg:text-left">
-              <h1 className="text-5xl font-bold">Login Now!</h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-            </div>
-            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-              <div className="card-body">
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="email"
-                    className="input input-bordered"
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Password</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="password"
-                    className="input input-bordered"
-                  />
-                  <label className="label">
-                    <Link to="/" href="#" className="label-text-alt link link-hover">
-                      Forgot password?
-                    </Link>
-                  </label>
-                </div>
-                <div className="form-control mt-6">
-                  <button className="btn btn-primary">Login</button>
-                </div>
+  return (
+    <div>
+      <div className="hero min-h-auto bg-base-100">
+        <div className="hero-content grid grid-cols-12 md:gap-6 lg:gap-12">
+          <div className="card shadow-2xl bg-base-100 col-span-12 md:col-span-6 ml-auto order-2 sm:order-1">
+            <div className="card-body">
+              
+              
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Email address"
+                  className="input input-bordered"
+                />
               </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+              </div>
+              <label className="label">
+                <Link to="/register">
+                  <p className="label-text link link-hover">
+                    Do not have an account?
+                    <span className="ml-5 underline tex-primary font-bold">
+                      Please Register
+                    </span>
+                  </p>
+                </Link>
+              </label>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Register</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-12 md:col-span-6 mr-auto order-1 sm:order-2">
+            <h1 className="text-5xl font-bold mb-4 text-primary">
+              Please Login!
+            </h1>
+            <div className="">
+                <p>Login and Enjoy Coding like Fun....!!</p>
             </div>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,78 +1,96 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-      <div>
-        <div className="hero min-h-auto bg-base-100">
-          <div className="hero-content grid md:grid-cols-5 md:gap-6 lg:gap-12">
-            <div className="card shadow-2xl bg-base-100 col-span-12 md:col-span-3 order-2 sm:order-1">
-              <div className="card-body">
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Name</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="Name"
-                    className="input input-bordered"
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Photo URL</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="Photo url"
-                    className="input input-bordered"
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="Email address"
-                    className="input input-bordered"
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Password</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="password"
-                    className="input input-bordered"
-                  />
-                </div>
-                <div className="form-control mt-6">
-                  <button className="btn btn-primary">Register</button>
-                </div>
+  return (
+    <div>
+      <div className="hero min-h-auto bg-base-100">
+        <div className="hero-content grid md:grid-cols-5 md:gap-6 lg:gap-12">
+          <div className="card shadow-2xl bg-base-100 col-span-12 md:col-span-3 order-2 sm:order-1">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  name="name"
+                  type="text"
+                  placeholder="Name"
+                  required
+                  className="input input-bordered"
+                />
               </div>
-            </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo URL</span>
+                </label>
+                <input
+                  type="text"
+                  name="photo"
+                  placeholder="Photo url"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  name="email"
+                  type="email"
+                  required
+                  placeholder="Email address"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                name="password"
+                  type="password"
+                  placeholder="password"
+                  required
+                  className="input input-bordered"
+                />
+              </div>
+              <label className="label">
+                <Link to="/login">
+                  <p className="label-text link link-hover">
+                    Already have an account?
+                    <span className="ml-5 underline tex-primary font-bold">
+                      Please Login
+                    </span>
+                  </p>
+                </Link>
+              </label>
+              <div className="form-control mt-6">
+                <input type="submit" className="btn btn-primary" value="Register"></input>
+              </div>
+            </form>
+          </div>
 
-            <div className="col-span-12 md:col-span-2 order-1 sm:order-2">
-              <h1 className="text-5xl font-bold mb-4 text-primary">
-                Register Now!
-              </h1>
-              <div className="">
-                <button className="btn bg-red-600 mx-auto md:mx-0 block mb-3">
-                  Sign Up With Google
-                </button>
-                <button className="btn block mb-3 mx-auto md:mx-0">
-                  Sign Up With Github
-                </button>
-                <button className="btn bg-blue-500 block mx-auto md:mx-0">
-                  Sign Up With FaceBook
-                </button>
-              </div>
+          <div className="col-span-12 md:col-span-2 order-1 sm:order-2">
+            <h1 className="text-5xl font-bold mb-4 text-primary">
+              Register Now!
+            </h1>
+            <div className="">
+              <button className="btn bg-red-600 mx-auto md:mx-0 block mb-3">
+                Sign Up With Google
+              </button>
+              <button className="btn block mb-3 mx-auto md:mx-0">
+                Sign Up With Github
+              </button>
+              <button className="btn bg-blue-500 block mx-auto md:mx-0">
+                Sign Up With FaceBook
+              </button>
             </div>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Register;
