5f20420125cdc3d15d4f48cbb0846f2e727846a1
Masum Billah <masumbillah6584@gmail.com>
2022-10-25 15:25:53 +0600

    added Navbar on header components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './components/Header/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>App is Running....</h1>
-    </div>
+    <>
+    <Header></Header>
+    </>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,103 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+      <div>
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li tabIndex={0}>
+                  <a className="justify-between">
+                    Parent
+                    <svg
+                      className="fill-current"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                    </svg>
+                  </a>
+                  <ul className="p-2">
+                    <li>
+                      <a>Submenu 1</a>
+                    </li>
+                    <li>
+                      <a>Submenu 2</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a>Item 3</a>
+                </li>
+              </ul>
+            </div>
+            <a className="btn btn-ghost normal-case text-xl">
+              <img src="logo.jpg" className="w-full h-full rounded-lg" alt="" />
+            </a>
+            <a className="btn btn-ghost normal-case text-xl">Coding With Fun</a>
+          </div>
+          <div className="navbar hidden lg:flex">
+            <ul className="menu menu-horizontal p-0">
+              <li>
+                <a>Courses</a>
+              </li>
+              <li tabIndex={0}>
+                <a>FAQ</a>
+              </li>
+              <li>
+                <a>Blog</a>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <ul className="menu menu-horizontal p-0">
+              <li>
+                <a>Login</a>
+              </li>
+              <li tabIndex={0}>
+                <a>Register</a>
+              </li>
+              <li>
+                <a>User Name</a>
+              </li>
+            </ul>
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  src="https://placeimg.com/80/80/people"
+                  alt="profilePicture"
+                />
+              </div>
+            </label>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Header;
