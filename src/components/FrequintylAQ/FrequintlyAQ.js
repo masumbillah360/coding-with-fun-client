7dc872d458edc1338ed34977d5461e50ab1ff5e3
Masum Billah <masumbillah6584@gmail.com>
2022-10-27 11:56:42 +0600

    update FAQ

--- src/components/FrequintylAQ/FrequintlyAQ.js
+++ src/components/FrequintylAQ/FrequintlyAQ.js
@@ -5,43 +5,49 @@ const FrequintlyAQ = () => {
       <div className='min-h-screen'>
         <div className="collapse collapse-arrow mb-3 rounded">
           <input type="checkbox" className="peer" />
-          <div className="collapse-title bg-base-200 text-base-content peer-checked:bg-accent-focus peer-checked:text-primary-content font-bold">
-            What is cors ?
+          <div className="collapse-title bg-base-200 text-base-content peer-checked:bg-slate-500 peer-checked:text-primary-content font-bold">
+            Who Made This Web App ?
           </div>
           <div className="collapse-content bg-light text-white-content peer-checked:bg-slate-100 peer-checked:text-black-content">
-            <p>hello</p>
+            <p className="mt-3">
+              This web app made by Masum Billah.
+            </p>
           </div>
         </div>
 
         <div className="collapse collapse-arrow mb-3 rounded">
           <input type="checkbox" className="peer" />
-          <div className="collapse-title bg-base-300 text-base-content peer-checked:bg-accent-focus peer-checked:text-primary-content font-bold">
-            Why are you using firebase? What other options do you have to
-            implement authentication ?
+          <div className="collapse-title bg-base-300 text-base-content peer-checked:bg-slate-500 peer-checked:text-primary-content font-bold">
+            Why make this app ?
           </div>
           <div className="collapse-content bg-light text-white-content peer-checked:bg-slate-100 peer-checked:text-black-content">
-            <p>hello</p>
+            <p className="mt-3">It's Make for testing my reactJs skill. It's totaly challages for me. I accept this challage and that's The result.</p>
           </div>
         </div>
 
         <div className="collapse collapse-arrow mb-3 rounded">
           <input type="checkbox" className="peer" />
-          <div className="collapse-title bg-base-200 text-base-content peer-checked:bg-accent-focus peer-checked:text-primary-content font-bold">
-            How does the private route work?
+          <div className="collapse-title bg-base-200 text-base-content peer-checked:bg-slate-500 peer-checked:text-primary-content font-bold">
+            Which Technology used for this app ?
           </div>
           <div className="collapse-content bg-light text-white-content peer-checked:bg-slate-100 peer-checked:text-black-content">
-            <p>hello</p>
+            <p className="mt-3">
+              <li>ReactJs</li>
+              <li>React Router</li>
+              <li>Firebase</li>
+              <li>Express Js</li>
+            </p>
           </div>
         </div>
 
 
         <div className="collapse collapse-arrow mb-3 rounded">
           <input type="checkbox" className="peer" />
-          <div className="collapse-title bg-base-300 text-base-content peer-checked:bg-accent-focus peer-checked:text-primary-content font-bold">
-            What is Node? How does Node work?
+          <div className="collapse-title bg-base-300 text-base-content peer-checked:bg-slate-500 peer-checked:text-primary-content font-bold">
+            Why React Js ?
           </div>
           <div className="collapse-content bg-light text-white-content peer-checked:bg-slate-100 peer-checked:text-black-content">
-            <p>hello</p>
+            <p className="mt-3">React Js is the most pupuler library for making user interface and it's so fast and components base. Thats why reactJs. </p>
           </div>
         </div>
       </div>
